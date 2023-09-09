3e7c14a8e1314bd6d7b414a8cf0a812076c1fbc6
bangms <seoyeon@DESKTOP-HB2LLRJ>
2023-09-10 00:22:03 +0900

    Main Page css modify

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,6 +1,7 @@
-import React, { useRef, useEffect } from "react";
-import { gsap } from "gsap";
-import { Link } from "react-router-dom";
+import React, { useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
+import { Link } from 'react-router-dom';
+import './Hamburger.scss';
 
 import {
   staggerText,
@@ -10,8 +11,8 @@ import {
   handleHoverExit,
   handleCityReturn,
   handleCity,
-  staggerRevealClose
-} from "./Animations";
+  staggerRevealClose,
+} from './Animations';
 
 const Hamburger = ({ state }) => {
   // Create varibles of our dom nodes
@@ -31,18 +32,15 @@ const Hamburger = ({ state }) => {
 
       staggerRevealClose(reveal2, reveal1);
       // Set menu to display none
-      gsap.to(menuLayer, { duration: 1, css: { display: "none" } });
-    } else if (
-      state.clicked === true ||
-      (state.clicked === true && state.initial === null)
-    ) {
+      gsap.to(menuLayer, { duration: 1, css: { display: 'none' } });
+    } else if (state.clicked === true || (state.clicked === true && state.initial === null)) {
       // Set menu to display block
-      gsap.to(menuLayer, { duration: 0, css: { display: "block" } });
+      gsap.to(menuLayer, { duration: 0, css: { display: 'block' } });
       //Allow menu to have height of 100%
       gsap.to([reveal1, reveal2], {
         duration: 0,
         opacity: 1,
-        height: "100%"
+        height: '100%',
       });
       staggerReveal(reveal1, reveal2);
       fadeInUp(info);
@@ -51,55 +49,53 @@ const Hamburger = ({ state }) => {
   }, [state]);
 
   return (
-    <div ref={el => (menuLayer = el)} className='hamburger-menu'>
-      <div
-        ref={el => (reveal1 = el)}
-        className='menu-secondary-background-color'></div>
-      <div ref={el => (reveal2 = el)} className='menu-layer'>
-        <div
-          ref={el => (cityBackground = el)}
-          className='menu-city-background'></div>
-        <div className='container'>
-          <div className='wrapper'>
-            <div className='menu-links'>
+    <div ref={(el) => (menuLayer = el)} className="hamburger-menu">
+      <div ref={(el) => (reveal1 = el)} className="menu-secondary-background-color"></div>
+      <div ref={(el) => (reveal2 = el)} className="menu-layer">
+        <div ref={(el) => (cityBackground = el)} className="menu-city-background"></div>
+        <div className="container">
+          <div className="wrapper">
+            <div className="menu-links">
               <nav>
                 <ul>
                   <li>
                     <Link
-                      onMouseEnter={e => handleHover(e)}
-                      onMouseOut={e => handleHoverExit(e)}
-                      ref={el => (line1 = el)}
-                      to='/about'>
+                      onMouseEnter={(e) => handleHover(e)}
+                      onMouseOut={(e) => handleHoverExit(e)}
+                      ref={(el) => (line1 = el)}
+                      to="/about"
+                    >
                       About
                     </Link>
                   </li>
                   <li>
                     <Link
-                      onMouseEnter={e => handleHover(e)}
-                      onMouseOut={e => handleHoverExit(e)}
-                      ref={el => (line2 = el)}
-                      to='/post'>
+                      onMouseEnter={(e) => handleHover(e)}
+                      onMouseOut={(e) => handleHoverExit(e)}
+                      ref={(el) => (line2 = el)}
+                      to="/post"
+                    >
                       Post
                     </Link>
                   </li>
                   <li>
                     <Link
-                      onMouseEnter={e => handleHover(e)}
-                      onMouseOut={e => handleHoverExit(e)}
-                      ref={el => (line3 = el)}
-                      to='/contact'>
+                      onMouseEnter={(e) => handleHover(e)}
+                      onMouseOut={(e) => handleHoverExit(e)}
+                      ref={(el) => (line3 = el)}
+                      to="/contact"
+                    >
                       Contact
                     </Link>
                   </li>
                 </ul>
               </nav>
-              <div ref={el => (info = el)} className='info'>
+              <div ref={(el) => (info = el)} className="info">
                 <h3>Our Promise</h3>
                 <p>
-                  The passage experienced a surge in popularity during the 1960s
-                  when Letraset used it on their dry-transfer sheets, and again
-                  during the 90s as desktop publishers bundled the text with
-                  their software.
+                  The passage experienced a surge in popularity during the 1960s when Letraset used it on their
+                  dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their
+                  software.
                 </p>
               </div>
             </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,18 +3,104 @@ import styled from 'styled-components';
 
 const Main = () => {
   const Container = styled.div`
-    display: flex;
-    height: 100vh;
     width: 100vw;
+    height: 100vh;
+    display: flex;
+  `;
+  const Title = styled.div`
+    display: flex;
+    height: calc(100vh- 100px);
+    width: 100vw;
+    justify-content: center;
+    align-items: center;
   `;
   const MainSection = styled.div`
-    padding: 140px 45px;
-    background: red;
-    width: 100%;
+    font-size: 2vmin;
+    font-family: JetBrains Mono, monospace;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    color: #e4bb68;
+    h1,
+    span {
+      font-family: 'JetBrains Mono', monospace;
+      margin: 0;
+      padding: 0;
+    }
+    .string {
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+      animation: move 4s infinite;
+    }
+
+    .greeting {
+      position: relative;
+      top: 10.5vmin;
+      animation: white-out 5s infinite;
+    }
+
+    .closure::after {
+      content: '';
+      position: absolute;
+      height: 25vmin;
+      width: 100%;
+      background: #fff;
+      transform: translate(-50%, -24.5vmin);
+    }
+
+    .closure::before {
+      content: '';
+      position: absolute;
+      height: 25vmin;
+      width: 100%;
+      background: #fff;
+      transform: translate(-50%, 5vmin);
+    }
+
+    .en {
+      color: #fa8231;
+    }
+
+    .es {
+      color: #a9b0bd;
+    }
+
+    .de {
+      color: #c678dd;
+    }
+    @keyframes move {
+      25% {
+        transform: translatey(-5.5vmin);
+        opacity: 1;
+      }
+      50% {
+        transform: translatey(-10.5vmin);
+      }
+      100% {
+        transform: translatey(-16vmin);
+      }
+    }
   `;
   return (
     <Container>
-      <MainSection>여기는 메인 페이지니까 경력 사항이랑..자기소개 같은거 적어놓쟈</MainSection>
+      <Title>
+        <MainSection>
+          <h1>
+            System
+            <span style={{ color: '#fff' }}>
+              .<span style={{ color: '#e06c75' }}>out</span>.
+            </span>
+            <span style={{ color: '#61afef' }}>println</span>("{' '}
+          </h1>
+          <div class="string">
+            <h1 class="greeting en">Welcome!</h1>
+            <h1 class="greeting es">Front-end Developer</h1>
+            <h1 class="greeting de">Bang's Portfolio</h1>
+          </div>
+          <h1 class="closure"> ");</h1>
+        </MainSection>
+      </Title>
     </Container>
   );
 };
